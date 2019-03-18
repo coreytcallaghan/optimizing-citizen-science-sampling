b518403a33bf1d11586e9b2142a276d263da0a4a
Corey Callaghan <callaghan.corey.t@gmail.com>
2019-03-19 07:26:12 +1100

    i think this is where the intermediate calculations has to be done (accounting for unsampled grid cells on a given day

--- R/create_model_df.R
+++ R/create_model_df.R
@@ -1,3 +1,9 @@
+library(dplyr)
+library(BBmisc)
+library(tidyr)
+
+
+
 # get a dataframe for modelling
 setwd("Data/Sampling dates 2018")
 
@@ -20,11 +26,6 @@ for (file in file_list){
   
 }
 
-dataset <- dataset %>% 
-  filter(complete.cases(.))
-
-
-
 setwd('..')
 setwd('..')
 
@@ -34,10 +35,31 @@ setwd('..')
 lev <- readRDS("Data/leverage_results.RDS")
 
 
-# final_df 
+lookup <- dataset %>%
+  dplyr::select(grid_id, LOCALITY_ID, SAMPLING_EVENT_IDENTIFIER, OBSERVATION_DATE) %>%
+  distinct()
+
 analysis_data <- dataset %>%
+  dplyr::select(-LOCALITY_ID, -SAMPLING_EVENT_IDENTIFIER, -dynamic_date) %>%
+  distinct() %>%
+  mutate(sampled=gsub("yes", 0, .$sampled)) %>%
+  mutate(sampled=gsub("no", 1, .$sampled)) %>%
+  mutate(sampled=as.numeric(as.character(.$sampled))) %>%
+  mutate(distance_sample=dist_km_nn*sampled) %>%
+  mutate(m_w_t=median_waiting_time*duration) %>%
+  mutate(m_w_t_n_n=neighbor_waiting_time) %>%
+  group_by(OBSERVATION_DATE) %>%
+  mutate(norm.distance_sample = normalize(distance_sample, method="range", range=c(0,100))) %>%
+  mutate(norm.m_w_t = normalize(m_w_t, method="range", range=c(0,100))) %>%
+  mutate(norm.m_w_t_n_n = normalize(m_w_t_n_n, method="range", range=c(0,100))) %>%
+  mutate(norm.days_since = normalize(days_since, method="range", range=c(0,100))) %>%
+  left_join(., lookup, by=c("grid_id", "OBSERVATION_DATE")) %>%
+  ungroup() %>%
+  filter(complete.cases(.)) %>%
   left_join(., lev, by=c("SAMPLING_EVENT_IDENTIFIER", "LOCALITY_ID", "OBSERVATION_DATE"))
 
+
+saveRDS(analysis_data, file="Data/Modelling data/analysis_data.RDS")
 
 
 
