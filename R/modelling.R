479aaa007b338126714610d3ff0a1b2d83e5e0b0
Corey Callaghan <callaghan.corey.t@gmail.com>
2019-03-19 07:32:45 +1100

    quick go at some modelling

---
+++ R/modelling.R
@@ -0,0 +1,44 @@
+### Modelling script
+
+# packages
+library(dplyr)
+library(ggplot2)
+library(BBmisc)
+
+
+# load data
+analysis_data <- readRDS("Data/Modelling data/analysis_data.RDS")
+
+load("Data/Spatial data/sydney_grids.RData")
+
+lev_results <- readRDS("Data/leverage_results.RDS")
+
+sites_grids_lookup <- readRDS("Data/sites_and_grids_lookup.RDS")
+
+# make df for modelling
+hist(analysis_data$DATE_dfbetas, breaks=30)
+
+hist(log(analysis_data$DATE_dfbetas), breaks=30)
+  
+# try a linear model first
+# no transform
+mod <- lm(DATE_dfbetas ~ norm.distance_sample + norm.days_since + norm.m_w_t_n_n + norm.m_w_t, data=analysis_data)
+
+par(mfrow=c(2, 2))
+plot(mod)
+
+summary(mod)
+
+# doesn't look good!
+# try a log-transform of response
+mod2 <- lm(log(DATE_dfbetas) ~ norm.distance_sample + norm.days_since + norm.m_w_t_n_n + norm.m_w_t, data=analysis_data)
+
+par(mfrow=c(2, 2))
+plot(mod2)
+
+summary(mod2)
+
+
+
+
+
