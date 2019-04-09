0e02f982cd1bb1ebeb648eb6055a5ad09d218029
Corey Callaghan <callaghan.corey.t@gmail.com>
2019-04-09 11:49:11 +1000

    scaling myself before hand and saving model objects. dropping duration from models

--- R/modelling.R
+++ R/modelling.R
@@ -10,6 +10,7 @@ library(lme4)
 library(standardize)
 library(tibble)
 library(visreg)
+library(arm)
 
 # load data
 lev <- readRDS("Data/Modelling data/leverage_results.RDS")
@@ -29,27 +30,70 @@ hist(analysis_data$DATE_dfbetas, breaks=30)
 
 hist(log(analysis_data$DATE_dfbetas), breaks=30)
 
+# rescale variables
+analysis_data <- analysis_data %>%
+  group_by(grid_size) %>%
+  mutate(s.median_waiting_time=arm::rescale(median_waiting_time)) %>%
+  mutate(s.duration=arm::rescale(duration)) %>%
+  mutate(s.Number_of_days_sampled=rescale(Number_of_days_sampled)) %>%
+  mutate(s.days_since=rescale(days_since)) %>%
+  mutate(s.dist_km_nn=rescale(dist_km_nn)) %>%
+  mutate(s.neighbor_waiting_time=rescale(neighbor_waiting_time))
+
 # look at correlation among predictor variables
-cor_object <- cor(dplyr::select(analysis_data, median_waiting_time, 
-                                duration, Number_of_days_sampled, days_since,
-                                dist_km_nn, neighbor_waiting_time))
-ggcorrplot(cor_object)
+cor_object5 <- analysis_data %>%
+  dplyr::filter(grid_size==5) %>%
+  dplyr::select(s.median_waiting_time, s.duration, s.Number_of_days_sampled, 
+                s.days_since, s.dist_km_nn, s.neighbor_waiting_time) %>%
+  ungroup()
+
+ggcorrplot(cor(na.omit(cor_object5[2:7])))+
+  ggtitle("5 km grid size")
+
+# look at correlation among predictor variables
+cor_object10 <- analysis_data %>%
+  dplyr::filter(grid_size==10) %>%
+  dplyr::select(s.median_waiting_time, s.duration, s.Number_of_days_sampled, 
+                s.days_since, s.dist_km_nn, s.neighbor_waiting_time) %>%
+  ungroup()
+
+ggcorrplot(cor(na.omit(cor_object10[2:7])))+
+  ggtitle("10 km grid size")
 
 
-# start with some very simple linear models
-# see whether or not a grid was sampled is important
-mod <- lm(log(DATE_dfbetas) ~ sampled, data=filter(analysis_data, grid_size==5))
+# look at correlation among predictor variables
+cor_object25 <- analysis_data %>%
+  dplyr::filter(grid_size==25) %>%
+  dplyr::select(s.median_waiting_time, s.duration, s.Number_of_days_sampled, 
+                s.days_since, s.dist_km_nn, s.neighbor_waiting_time) %>%
+  ungroup()
 
-par(mfrow=c(2, 2))
-plot(mod)
+ggcorrplot(cor(na.omit(cor_object25[2:7])))+
+  ggtitle("25 km grid size")
 
-summary(mod)
+
+# look at correlation among predictor variables
+cor_object50 <- analysis_data %>%
+  dplyr::filter(grid_size==50) %>%
+  dplyr::select(s.median_waiting_time, s.duration, s.Number_of_days_sampled, 
+                s.days_since, s.dist_km_nn, s.neighbor_waiting_time) %>%
+  ungroup()
+
+ggcorrplot(cor(na.omit(cor_object50[2:6])))+
+  ggtitle("50 km grid size")
+
+
+
+## Based on all that I think we should get rid of 'duration'
+## as it appears to be the most correlated with both median waiting time
+## and correlated with days since
+### Run a model for each of the grid sizes
 
 # 5 km grid
 analysis_data.5 <- analysis_data %>% dplyr::filter(grid_size==5)
 # see about median sampling interval
-mod5 <- lm(log(DATE_dfbetas) ~ median_waiting_time + duration + Number_of_days_sampled +
-            days_since + dist_km_nn + neighbor_waiting_time, data=analysis_data.5)
+mod5 <- lm(log(DATE_dfbetas) ~ s.median_waiting_time + s.Number_of_days_sampled +
+            s.days_since + s.dist_km_nn + s.neighbor_waiting_time, data=analysis_data.5)
 
 par(mfrow=c(2, 2))
 plot(mod5)
@@ -58,24 +102,20 @@ summary(mod5)
 anova(mod5)
 
 
-standard5 <- arm::standardize(mod5)
-summary(standard5)
-
-
-estimates_5 <- as.data.frame(standard5$coefficients) %>%
+estimates_5 <- as.data.frame(mod5$coefficients) %>%
   rownames_to_column(var="params") %>%
-  rename(standard_estimate = `standard5$coefficients`) %>%
-  mutate(lwr_conf=as.data.frame(confint(standard5))[,1]) %>%
-  mutate(upr_conf=as.data.frame(confint(standard5))[,2]) %>%
-  mutate(p_value=summary(standard5)$coefficients[,4]) %>%
+  rename(standard_estimate = `mod5$coefficients`) %>%
+  mutate(lwr_conf=as.data.frame(confint(mod5))[,1]) %>%
+  mutate(upr_conf=as.data.frame(confint(mod5))[,2]) %>%
+  mutate(p_value=summary(mod5)$coefficients[,4]) %>%
   mutate(grid_size=5)
 
 
 # 10 km grid
 analysis_data.10 <- analysis_data %>% dplyr::filter(grid_size==10)
 # see about median sampling interval
-mod10 <- lm(log(DATE_dfbetas) ~ median_waiting_time + duration + Number_of_days_sampled +
-             days_since + dist_km_nn + neighbor_waiting_time, data=analysis_data.10)
+mod10 <- lm(log(DATE_dfbetas) ~ s.median_waiting_time + s.Number_of_days_sampled +
+             s.days_since + s.dist_km_nn + s.neighbor_waiting_time, data=analysis_data.10)
 
 par(mfrow=c(2, 2))
 plot(mod10)
@@ -83,24 +123,19 @@ plot(mod10)
 summary(mod10)
 anova(mod10)
 
-
-standard10 <- arm::standardize(mod10)
-summary(standard10)
-
-
-estimates_10 <- as.data.frame(standard10$coefficients) %>%
+estimates_10 <- as.data.frame(mod10$coefficients) %>%
   rownames_to_column(var="params") %>%
-  rename(standard_estimate = `standard10$coefficients`) %>%
-  mutate(lwr_conf=as.data.frame(confint(standard10))[,1]) %>%
-  mutate(upr_conf=as.data.frame(confint(standard10))[,2]) %>%
-  mutate(p_value=summary(standard10)$coefficients[,4]) %>%
+  rename(standard_estimate = `mod10$coefficients`) %>%
+  mutate(lwr_conf=as.data.frame(confint(mod10))[,1]) %>%
+  mutate(upr_conf=as.data.frame(confint(mod10))[,2]) %>%
+  mutate(p_value=summary(mod10)$coefficients[,4]) %>%
   mutate(grid_size=10)
 
 # 25 km grid
 analysis_data.25 <- analysis_data %>% dplyr::filter(grid_size==25)
 # see about median sampling interval
-mod25 <- lm(log(DATE_dfbetas) ~ median_waiting_time + duration + Number_of_days_sampled +
-              days_since + dist_km_nn + neighbor_waiting_time, data=analysis_data.25)
+mod25 <- lm(log(DATE_dfbetas) ~ s.median_waiting_time + s.Number_of_days_sampled +
+              s.days_since + s.dist_km_nn + s.neighbor_waiting_time, data=analysis_data.25)
 
 par(mfrow=c(2, 2))
 plot(mod25)
@@ -108,24 +143,19 @@ plot(mod25)
 summary(mod25)
 anova(mod25)
 
-
-standard25 <- arm::standardize(mod25)
-summary(standard25)
-
-
-estimates_25 <- as.data.frame(standard25$coefficients) %>%
+estimates_25 <- as.data.frame(mod25$coefficients) %>%
   rownames_to_column(var="params") %>%
-  rename(standard_estimate = `standard25$coefficients`) %>%
-  mutate(lwr_conf=as.data.frame(confint(standard25))[,1]) %>%
-  mutate(upr_conf=as.data.frame(confint(standard25))[,2]) %>%
-  mutate(p_value=summary(standard25)$coefficients[,4]) %>%
+  rename(standard_estimate = `mod25$coefficients`) %>%
+  mutate(lwr_conf=as.data.frame(confint(mod25))[,1]) %>%
+  mutate(upr_conf=as.data.frame(confint(mod25))[,2]) %>%
+  mutate(p_value=summary(mod25)$coefficients[,4]) %>%
   mutate(grid_size=25)
 
 # 50 km grid
 analysis_data.50 <- analysis_data %>% dplyr::filter(grid_size==50)
 # see about median sampling interval
-mod50 <- lm(log(DATE_dfbetas) ~ median_waiting_time + duration + Number_of_days_sampled +
-              days_since + dist_km_nn + neighbor_waiting_time, data=analysis_data.50)
+mod50 <- lm(log(DATE_dfbetas) ~ s.median_waiting_time + s.Number_of_days_sampled +
+              s.days_since + s.dist_km_nn, data=analysis_data.50)
 
 par(mfrow=c(2, 2))
 plot(mod50)
@@ -133,17 +163,12 @@ plot(mod50)
 summary(mod50)
 anova(mod50)
 
-
-standard50 <- arm::standardize(mod50)
-summary(standard50)
-
-
-estimates_50 <- as.data.frame(standard50$coefficients) %>%
+estimates_50 <- as.data.frame(mod50$coefficients) %>%
   rownames_to_column(var="params") %>%
-  rename(standard_estimate = `standard50$coefficients`) %>%
-  mutate(lwr_conf=as.data.frame(confint(standard50))[,1]) %>%
-  mutate(upr_conf=as.data.frame(confint(standard50))[,2]) %>%
-  mutate(p_value=summary(standard50)$coefficients[,4]) %>%
+  rename(standard_estimate = `mod50$coefficients`) %>%
+  mutate(lwr_conf=as.data.frame(confint(mod50))[,1]) %>%
+  mutate(upr_conf=as.data.frame(confint(mod50))[,2]) %>%
+  mutate(p_value=summary(mod50)$coefficients[,4]) %>%
   mutate(grid_size=50)
 
 
@@ -154,13 +179,12 @@ bind_rows(estimates_5, estimates_10, estimates_25, estimates_50) %>%
     p_value < 0.5 ~ "Significant",
     p_value > 0.5 ~ "Not-significant"
   )) %>%
-  ggplot(., aes(x=params, y=standard_estimate, shape=significant))+
+  ggplot(., aes(x=params, y=standard_estimate, color=as.character(grid_size), shape=significant))+
   geom_point()+
   geom_errorbar(aes(ymin=lwr_conf, ymax=upr_conf))+
   coord_flip()+
-  facet_wrap(~grid_size)+
   theme_bw()+
-  geom_hline(yintercept=0, color="red")+
+  geom_hline(yintercept=0, color="red", size=2)+
   xlab("")+
   ylab("Standardized parameter estimate")
 
@@ -181,11 +205,40 @@ visreg(standard50, "z.days_since")
 
 
 
+# write out each model object
+save(mod5, file="Data/Modelling data/model_5_km_object.RData")
+save(mod10, file="Data/Modelling data/model_10_km_object.RData")
+save(mod25, file="Data/Modelling data/model_25_km_object.RData")
+save(mod50, file="Data/Modelling data/model_50_km_object.RData")
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# start with some very simple linear models
+# see whether or not a grid was sampled is important
+mod <- lm(log(DATE_dfbetas) ~ sampled, data=filter(analysis_data, grid_size==5))
+
+par(mfrow=c(2, 2))
+plot(mod)
+
+summary(mod)
 
 
 
